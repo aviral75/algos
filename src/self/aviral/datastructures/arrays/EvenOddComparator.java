d6d9a0cff0391c5b60ae1f8ecfd54d46d3a16d11
Aviral Shukla <aviral75@gmail.com>
2019-11-30 23:10:21 -0800

    space added

--- src/self/aviral/datastructures/arrays/EvenOddComparator.java
+++ src/self/aviral/datastructures/arrays/EvenOddComparator.java
@@ -18,8 +18,8 @@ public class EvenOddComparator {
 
 		@Override
 		public int compare(Integer o1, Integer o2) {
-			if(o1%2==0 && o2%2!=0) return -1;
-			if(o1%2!=0 && o2%2==0) return 1;
+			if(o1%2 == 0 && o2%2 != 0) return -1;
+			if(o1%2 != 0 && o2%2 == 0) return 1;
 			return o1-o2;
 		}
 
